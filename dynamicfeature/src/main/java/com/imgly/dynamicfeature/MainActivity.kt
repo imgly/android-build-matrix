737578b44edb56ded7bc81b68ca1a6cc1346e637
Prempal Singh <prempal.42@gmail.com>
2022-04-06 18:19:00 +0530

    create dfm example

---
+++ dynamicfeature/src/main/java/com/imgly/dynamicfeature/MainActivity.kt
@@ -0,0 +1,183 @@
+package com.imgly.dynamicfeature
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ly.img.android.SourceType
+import ly.img.android.pesdk.PhotoEditorSettingsList
+import ly.img.android.pesdk.VideoEditorSettingsList
+import ly.img.android.pesdk.assets.filter.basic.FilterPackBasic
+import ly.img.android.pesdk.assets.font.basic.FontPackBasic
+import ly.img.android.pesdk.assets.frame.basic.FramePackBasic
+import ly.img.android.pesdk.assets.overlay.basic.OverlayPackBasic
+import ly.img.android.pesdk.assets.sticker.emoticons.StickerPackEmoticons
+import ly.img.android.pesdk.assets.sticker.shapes.StickerPackShapes
+import ly.img.android.pesdk.backend.model.EditorSDKResult
+import ly.img.android.pesdk.backend.model.state.LoadSettings
+import ly.img.android.pesdk.backend.model.state.PhotoEditorSaveSettings
+import ly.img.android.pesdk.backend.model.state.VideoEditorSaveSettings
+import ly.img.android.pesdk.ui.activity.PhotoEditorBuilder
+import ly.img.android.pesdk.ui.activity.VideoEditorBuilder
+import ly.img.android.pesdk.ui.model.state.UiConfigFilter
+import ly.img.android.pesdk.ui.model.state.UiConfigFrame
+import ly.img.android.pesdk.ui.model.state.UiConfigOverlay
+import ly.img.android.pesdk.ui.model.state.UiConfigSticker
+import ly.img.android.pesdk.ui.model.state.UiConfigText
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val PESDK_RESULT = 1
+        const val VESDK_RESULT = 2
+        const val GALLERY_RESULT = 3
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_take_photo).setOnClickListener {
+            selectPhoto()
+        }
+
+        findViewById<Button>(R.id.btn_take_video).setOnClickListener {
+            selectVideo()
+        }
+    }
+
+    private fun selectPhoto() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        try {
+            startActivityForResult(intent, GALLERY_RESULT)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(
+                this,
+                "No Gallery APP installed",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun selectVideo() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.setDataAndType(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, "video/*")
+
+        try {
+            startActivityForResult(intent, GALLERY_RESULT)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(
+                this,
+                "No Gallery APP installed",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun createPESDKSettingsList() =
+        PhotoEditorSettingsList(false)
+            .configure<UiConfigFilter> {
+                it.setFilterList(FilterPackBasic.getFilterPack())
+            }
+            .configure<UiConfigText> {
+                it.setFontList(FontPackBasic.getFontPack())
+            }
+            .configure<UiConfigFrame> {
+                it.setFrameList(FramePackBasic.getFramePack())
+            }
+            .configure<UiConfigOverlay> {
+                it.setOverlayList(OverlayPackBasic.getOverlayPack())
+            }
+            .configure<UiConfigSticker> {
+                it.setStickerLists(
+                    StickerPackEmoticons.getStickerCategory(),
+                    StickerPackShapes.getStickerCategory()
+                )
+            }
+            .configure<PhotoEditorSaveSettings> {
+                it.setOutputToGallery(Environment.DIRECTORY_DCIM)
+            }
+
+    private fun createVesdkSettingsList() =
+        VideoEditorSettingsList(false)
+            .configure<UiConfigFilter> {
+                it.setFilterList(FilterPackBasic.getFilterPack())
+            }
+            .configure<UiConfigText> {
+                it.setFontList(FontPackBasic.getFontPack())
+            }
+            .configure<UiConfigFrame> {
+                it.setFrameList(FramePackBasic.getFramePack())
+            }
+            .configure<UiConfigOverlay> {
+                it.setOverlayList(OverlayPackBasic.getOverlayPack())
+            }
+            .configure<UiConfigSticker> {
+                it.setStickerLists(
+                    StickerPackEmoticons.getStickerCategory(),
+                    StickerPackShapes.getStickerCategory()
+                )
+            }
+            .configure<VideoEditorSaveSettings> {
+                it.setOutputToGallery(Environment.DIRECTORY_DCIM)
+            }
+
+    private fun openEditor(source: Uri?) {
+        val sourceType = SourceType.detectTypeSafe(source)
+        if (sourceType == SourceType.IMAGE) {
+            val settingsList = createPESDKSettingsList()
+
+            settingsList.configure<LoadSettings> {
+                it.source = source
+            }
+
+            PhotoEditorBuilder(this)
+                .setSettingsList(settingsList)
+                .startActivityForResult(this, PESDK_RESULT)
+
+            settingsList.release()
+        } else {
+            val settingsList = createVesdkSettingsList()
+
+            settingsList.configure<LoadSettings> {
+                it.source = source
+            }
+
+            VideoEditorBuilder(this)
+                .setSettingsList(settingsList)
+                .startActivityForResult(this, VESDK_RESULT)
+
+            settingsList.release()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        intent ?: return
+
+        if (resultCode == RESULT_OK && requestCode == GALLERY_RESULT) {
+            // Open Editor with some uri in this case with an image selected from the system gallery.
+            openEditor(intent.data)
+        } else if (resultCode == RESULT_OK && (requestCode == PESDK_RESULT || requestCode == VESDK_RESULT)) {
+            // Editor has saved an Image.
+            val data = EditorSDKResult(intent)
+
+            Log.i("PESDK", "Source image is located here ${data.sourceUri}")
+            Log.i("PESDK", "Result image is located here ${data.resultUri}")
+
+            // TODO: Do something with the result image
+        } else if (resultCode == RESULT_CANCELED && (requestCode == PESDK_RESULT || requestCode == VESDK_RESULT)) {
+            // Editor was canceled
+            val data = EditorSDKResult(intent)
+
+            val sourceURI = data.sourceUri
+            // TODO: Do something with the source...
+        }
+    }
+}
